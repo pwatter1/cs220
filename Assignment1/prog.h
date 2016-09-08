9859b15c9cadfa430ffbf1fde931fd6e37a9ebd9
Patrick Watters <pwatter1@cs.binghamton.edu>
2016-09-07 21:04:10 -0400

    adding files in folder

---
+++ Assignment1/prog.h
@@ -0,0 +1,9 @@
+/* 
+ * Function that accepts integer "n" as input and returns nth fibonacci number. 
+ */ 
+int nthFib(int n);
+
+/*
+ * Function that returns square of a minus square of b
+ */
+int asq_minus_bsq(int a, int b);
